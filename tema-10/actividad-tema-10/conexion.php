1986e6838e7fb6f51215cdf2334d13558d2fc740
dgutper960 <dgutper960@outlook.es>
2024-01-17 12:10:00 +0100

    funcionalidad completa, revision modificar tabla dinámica

--- tema-10/actividad-tema-10/conexion.php
+++ tema-10/actividad-tema-10/conexion.php
@@ -1,100 +1,75 @@
 <?php
 
 /**
- * Clase conexion
- * - Esta clase será comun en todos los proyectos
+ * Clase Conexion
+ * - Esta clase será común en todos los proyectos
  */
 
 class Conexion
 {
-    # unico atributo -> visibilidad protegida
+    # Único atributo -> visibilidad protegida
     protected $pdo;
-
-    public $id;
+    public $id = null;
 
     # Constructor
     public function __construct()
     {
-
         try {
-
             $dsn = "mysql:host=localhost;dbname=tema10";
-
             $options = [
-
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_PERSISTENT => false,
                 PDO::ATTR_EMULATE_PREPARES => false,
                 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
-
             ];
 
             $this->pdo = new PDO($dsn, 'root', null, $options);
             $this->id = $_GET['id'];
 
-
         } catch (PDOException $e) {
             echo 'FALLO DE CONEXIÓN';
             exit();
         }
-
         // echo 'Conexión realizada con éxito';
-
     }
-
-
 
     /**
      * Creamos los métodos directamente
      */
+
     // Mostrar todas las personas
     public function getPersonas()
     {
-        // realizamos la consulta sql
+        // Realizamos la consulta SQL
         $sql = "SELECT id, nombre FROM tema10.datos";
 
         $stmt = $this->pdo->prepare($sql);
-
         $stmt->execute();
 
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         echo json_encode($result);
-
     }
 
     public function getIndividuo(int $id)
     {
-
         $sql = "SELECT * FROM tema10.datos WHERE id = :id";
-
         $stmt = $this->pdo->prepare($sql);
-
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-
         $stmt->execute();
 
         $result = $stmt->fetchAll(PDO::FETCH_OBJ);
 
         echo json_encode($result);
-
-
     }
-
-
 }
-
-
 
 $conexion = new Conexion();
 
-echo $conexion->getIndividuo(2);
-
-if($conexion->id == ''){
+if ($conexion->id == null) {
     $conexion->getPersonas();
-}else{
-    $conexion->getIndividuo($conexion->id);
-}
-
-
+  } else {
+    $conexion->getIndividuo((int)$conexion->id);
+  }
+  
 ?>
