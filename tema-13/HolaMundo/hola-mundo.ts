b56c4e57c672c19eedda986172e90d43c515a3d3
dgutper960 <dgutper960@outlook.es>
2024-01-31 12:16:07 +0100

    comentarios

--- tema-13/HolaMundo/hola-mundo.ts
+++ tema-13/HolaMundo/hola-mundo.ts
@@ -1,3 +1,10 @@
 var x : string;
 x='Hola mundo';
 console.log(x);
+
+/**
+ * Se debe transpilar previamente a JS
+ * -> Controlando el dierctorio dónde estamos
+ * -> Consola-> tsc  -- init
+ * -> Consola-> tsc nombre.ts
+ */
