316c4b4fb0d8edf27b6cd7eaa56c8c1ad2b2a057
dgutper960 <dgutper960@outlook.es>
2024-03-07 10:53:32 +0100

    DOM nativo Angular

--- tema_15/actividades/15.4_ajax/src/app/botonajax/botonajax.component.ts
+++ tema_15/actividades/15.4_ajax/src/app/botonajax/botonajax.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { ServiajaxService } from '../serviajax.service'
+import { ServiajaxService } from '../serviajax.service';
+
+import {OnInit ,Renderer2 ,ViewChild, ElementRef,} from '@angular/core';
 
 @Component({
   selector: 'app-botonajax',
@@ -9,17 +11,17 @@ import { ServiajaxService } from '../serviajax.service'
 })
 export class BotonajaxComponent {
 
-  constructor(private serviAjax: ServiajaxService ) { }
+  @ViewChild("datos") idDiv: ElementRef|any;
+
+  constructor(private serviAjax: ServiajaxService, private rd: Renderer2 ) { }
 
   llamaAjaxC():void{
     this.serviAjax.llamaAjax('http://localhost/dwec/tema_15/actividades/15.4_ajax/src/jsonGET.php?nombre=David&ciudad=Ubrique')
-      .subscribe(data=> {
-      var i:HTMLElement|null=document.getElementById('datos');
-      if(i!=null){
-          i.innerHTML=`Desde servidor ${data['nombre']} de ${data['ciudad']}`
-      };              
-    });
-  }
+      .subscribe(data=>{
+        this.rd.setProperty(this.idDiv.nativeElement, 'innerHTML',`Desde servidor ${data['nombre']} de ${data['ciudad']}`)
+         
+        }) ;      
+ }
 
 
 }
