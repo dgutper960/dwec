5b7342abe2b0cd661e1857311e34e5e28cc65450
dgutper960 <dgutper960@outlook.es>
2024-03-07 11:29:47 +0100

    añade estilos y configura import

--- tema_15/actividades/15.5_ngBootPrime/src/app/app.component.ts
+++ tema_15/actividades/15.5_ngBootPrime/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    NgbAccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
